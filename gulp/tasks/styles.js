91f6624417b6bddedb91a730182bf7bbefedf9b5
Jonathan <jtellkampf@gmail.com>
2019-11-11 19:32:21 +0000

    Completed Header Styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const cssvars = require("postcss-simple-vars");
 const nested = require("postcss-nested");
 const cssImport = require("postcss-import");
 const mixins = require("postcss-mixins");
+const hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", () => {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on("error", err => {
       console.log(err.toString());
       // this.emit('end')
